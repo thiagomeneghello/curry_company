ebe6fcdb35bdebcea8cedf57463dd3c138386d19
Thiago Meneghello <125320742+thiagomeneghello@users.noreply.github.com>
2023-08-17 20:34:17 -0300

    Update 1_visao_empresa.py

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -152,8 +152,8 @@ df1 = clean_code( df_raw )
 #===============================
 st.header(" Marketplace - Visão Cliente")
 
-image_path = "C:/Users/Thiago/Desktop/DADOS/repos/ftc_programacao_python/logo1.png"
-image = Image.open( image_path)
+#image_path = "C:/Users/Thiago/Desktop/DADOS/repos/ftc_programacao_python/logo1.png"
+image = Image.open( 'logo1.png' )
 st.sidebar.image( image, width=120 )
 
 st.sidebar.markdown("# Curry Company")
